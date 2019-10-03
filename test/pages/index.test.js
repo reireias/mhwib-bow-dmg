555523e94f871490fa8cf59b97c35ca54eb6921d
reireias <reireias@gmail.com>
2019-10-03 21:41:51 +0900

    add dummy test

---
+++ test/pages/index.test.js
@@ -0,0 +1,21 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import target from '@/pages/index'
+
+Vue.use(Vuetify)
+const localVue = createLocalVue()
+localVue.use(Vuetify)
+
+describe('index', () => {
+  it('should be vue instance', () => {
+    const vuetify = new Vuetify()
+    const wrapper = shallowMount(target, {
+      localVue,
+      vuetify
+    })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+    // TODO
+    // expect(wrapper.html()).toMatchSnapshot()
+  })
+})
