77a7490e83b33661dc1f1a4209db9881923bdd14
reireias <reireias@gmail.com>
2019-10-05 12:25:35 +0900

    update nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
         router: {
-          base: '/vuetifier/'
+          base: '/mhwib-bow-dmg/'
         }
       }
     : {}
