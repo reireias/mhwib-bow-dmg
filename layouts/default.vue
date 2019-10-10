84eb8311e35162c1e9ef76da43407de460c8d93d
reireias <reireias@gmail.com>
2019-10-10 17:49:32 +0900

    update loading page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,29 @@
 <template>
   <v-app>
-    <nuxt></nuxt>
+    <template v-if="show">
+      <nuxt></nuxt>
+    </template>
+    <template v-else>
+      <div style="height: 100%; display: table">
+        <h2
+          style="text-align: center; vertical-align: middle; height: 100%; display: table-cell"
+        >
+          Loading...
+        </h2>
+      </div>
+    </template>
   </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  mounted() {
+    this.show = true
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,29 +3,24 @@
     <v-row>
       <h1>弓専用ダメージ計算機(仮)</h1>
     </v-row>
-    <template v-if="show">
-      <v-row>
-        <weapon></weapon>
-      </v-row>
-      <v-row>
-        <buff></buff>
-      </v-row>
-      <v-row>
-        <skill></skill>
-      </v-row>
-      <v-row>
-        <motion></motion>
-      </v-row>
-      <v-row>
-        <target></target>
-      </v-row>
-      <v-row>
-        <result></result>
-      </v-row>
-    </template>
-    <template v-else>
-      Loading...
-    </template>
+    <v-row>
+      <weapon></weapon>
+    </v-row>
+    <v-row>
+      <buff></buff>
+    </v-row>
+    <v-row>
+      <skill></skill>
+    </v-row>
+    <v-row>
+      <motion></motion>
+    </v-row>
+    <v-row>
+      <target></target>
+    </v-row>
+    <v-row>
+      <result></result>
+    </v-row>
   </v-container>
 </template>
 
@@ -48,10 +43,6 @@ export default {
   },
   data() {
     return { show: false }
-  },
-  mounted() {
-    console.log('page mounted')
-    this.show = true
   }
 }
 </script>
