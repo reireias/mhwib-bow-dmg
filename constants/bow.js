53713dedb58224986fb493e9bbca81dbb23b983a
reireias <reireias@gmail.com>
2019-10-12 00:34:03 +0900

    add bow

--- constants/bow.js
+++ constants/bow.js
@@ -64,6 +64,54 @@ const bows = [
     parts: false
   },
   {
+    name: 'ワイズハンターボウⅢ',
+    attack: 312,
+    affinity: 0,
+    element: {
+      type: 'water',
+      value: 390,
+      hidden: true
+    },
+    coating: {
+      crossRange: true,
+      power: false
+    },
+    rare: 11,
+    parts: true
+  },
+  {
+    name: 'ラグーナシュートⅡ',
+    attack: 288,
+    affinity: 0,
+    element: {
+      type: 'water',
+      value: 330,
+      hidden: false
+    },
+    coating: {
+      crossRange: true,
+      power: true
+    },
+    rare: 10,
+    parts: true
+  },
+  {
+    name: '王牙弓【稚雷】',
+    attack: 324,
+    affinity: 0,
+    element: {
+      type: 'thunder',
+      value: 240,
+      hidden: false
+    },
+    coating: {
+      crossRange: true,
+      power: true
+    },
+    rare: 12,
+    parts: false
+  },
+  {
     name: '万雷弓【羽々矢】',
     attack: 264,
     affinity: 15,
@@ -80,6 +128,22 @@ const bows = [
     parts: true
   },
   {
+    name: '氷妖イヴェリア',
+    attack: 336,
+    affinity: 0,
+    element: {
+      type: 'ice',
+      value: 240,
+      hidden: false
+    },
+    coating: {
+      crossRange: true,
+      power: true
+    },
+    rare: 12,
+    parts: false
+  },
+  {
     name: 'ミスト＝グレイシア',
     attack: 276,
     affinity: 0,
@@ -94,6 +158,38 @@ const bows = [
     },
     rare: 11,
     parts: false
+  },
+  {
+    name: 'アイシクルブリザードⅡ',
+    attack: 276,
+    affinity: 25,
+    element: {
+      type: 'ice',
+      value: 300,
+      hidden: false
+    },
+    coating: {
+      crossRange: true,
+      power: true
+    },
+    rare: 10,
+    parts: true
+  },
+  {
+    name: '屍弓ヴァルヴェロス',
+    attack: 300,
+    affinity: 0,
+    element: {
+      type: 'dragon',
+      value: 330,
+      hidden: false
+    },
+    coating: {
+      crossRange: true,
+      power: false
+    },
+    rare: 11,
+    parts: true
   },
   {
     name: '龍封じの古弓Ⅱ',
