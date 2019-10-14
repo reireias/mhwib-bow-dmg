5c96c6367eeac891baea008190b2ba41251cb460
reireias <reireias@gmail.com>
2019-10-14 12:41:52 +0900

    update parts custom

--- constants/parts.js
+++ constants/parts.js
@@ -1,6 +1,6 @@
 const parts = [
-  { name: '属性Lv5 [ +50 ]', value: { attack: 0, affinity: 0, element: 50 } },
-  { name: '会心Lv5 [ +5 ]', value: { attack: 0, affinity: 5, element: 0 } },
-  { name: '攻撃Lv5 [ +5 ]', value: { attack: 5, affinity: 0, element: 0 } }
+  { name: '属性Lv6 [ +60 ]', value: { attack: 0, affinity: 0, element: 60 } },
+  { name: '会心Lv6 [ +6 ]', value: { attack: 0, affinity: 6, element: 0 } },
+  { name: '攻撃Lv6 [ +6 ]', value: { attack: 6, affinity: 0, element: 0 } }
 ]
 export default parts
