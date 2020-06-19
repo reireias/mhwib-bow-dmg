07ebce8713bc16ad433063786a5023fa5f8e56d8
reireias <reireias@users.noreply.github.com>
2020-06-19 07:36:01 +0000

    deploy: c1be2fd43aa83634bf062e7ca721933c1ab86ab2

--- sw.js
+++ sw.js
@@ -6,24 +6,24 @@ workbox.precaching.precacheAndRoute([
     "revision": "d1840162da66fc8fcfe6409c4e267350"
   },
   {
-    "url": "/mhwib-bow-dmg/_nuxt/4f4f428060182376e037.js",
-    "revision": "ec47b56bd70afd30dc34ce4074f94904"
-  },
-  {
-    "url": "/mhwib-bow-dmg/_nuxt/74b4f1c355829f14bbd9.js",
-    "revision": "9b7c704c616d64241daf7f4ded1e78c6"
-  },
-  {
     "url": "/mhwib-bow-dmg/_nuxt/83cfa10a5348789f3eca.js",
     "revision": "ea219a4b6b14f32175fbb01b7404eed9"
   },
   {
-    "url": "/mhwib-bow-dmg/_nuxt/bc1011c88697a83058ef.js",
-    "revision": "ae52f6496d1f4dc0ce2f249bc134643d"
+    "url": "/mhwib-bow-dmg/_nuxt/d0433a4e631e2c240361.js",
+    "revision": "9b7c704c616d64241daf7f4ded1e78c6"
   },
   {
     "url": "/mhwib-bow-dmg/_nuxt/d183e28fead23637ee9d.js",
     "revision": "3bcd7b67219cf851404f9e8ac038c489"
+  },
+  {
+    "url": "/mhwib-bow-dmg/_nuxt/dee9ccb040bc0c0c72c6.js",
+    "revision": "ec47b56bd70afd30dc34ce4074f94904"
+  },
+  {
+    "url": "/mhwib-bow-dmg/_nuxt/f786cdf00faa66c3db0e.js",
+    "revision": "bec8c1a2e98d93edefc43b60bae35ba6"
   }
 ], {
   "cacheId": "mhwib-bow-dmg",
